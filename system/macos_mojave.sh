4619ea6691740871f1bf1ab312c30bfb8165d39d
Karl Holub <holub008@umn.edu>
2019-03-25 14:19:55 -0500

    properly locating virtualenvs

--- system/macos_mojave.sh
+++ system/macos_mojave.sh
@@ -49,6 +49,7 @@ brew cask install sublime-text
 brew install python
 echo "alias python='python3'" >> ~/.bash_profile
 brew install pipenv
+echo "export PIPENV_VENV_IN_PROJECT=1" >> ~/.bash_profile
 brew install postgresql
 # postgresql had the following caveats:
 ==> icu4c
